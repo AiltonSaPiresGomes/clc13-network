4fc1884fbb65a3b73d5ad6ba9b005a625e58e6d4
EC2 Default User <ec2-user@ip-172-31-39-47.ec2.internal>
2025-04-15 01:32:31 +0000

    remocendo secrets

--- providers.tf
+++ providers.tf
@@ -10,7 +10,4 @@ terraform {
 provider "aws" {
   # Configuration options
   region     = "us-east-1"
-  access_key = "ASIA3IABUAJ5SZKRNBKP"
-  secret_key = "/qLaoaW8hctUsXNhoq2En0RpkcPyUwMRyL0ibEB6"
-  token = "IQoJb3JpZ2luX2VjEJf//////////wEaCXVzLXdlc3QtMiJIMEYCIQCqKEryNe8LUW+xHgzYqlpjPXSb+sfizsh/gpOv4jqMdQIhAJ5CetLJEbE4b3UsUfVX/xxl1LZEuUWW/jfE09bneJZXKq8CCB8QABoMNzczMDk3NTIxNzg3IgwZ6ekcR5D1lMfKujwqjAILEiSr116fQJCbKtpKMpmBk2iRafEpAVlOsg90WfQW1Nu7JjYIheEii/NSm2drV6lVgmEUKlqDQigpAesIa13JQ6jVI0aBKAlRG+2Ph89ILwgi/XY3CR917R6S7Pcwwq2Kq/EwVwBB5YHjFYscwtoRATvXfn/lWmKGAjJRFUX87mqqaUjcBPPhrsuTHIddf9Ec7T1gukPGwJbJOdH38/YkpHfCdyJKg1OxwLJMqVYwZwv9Y5rXwaeTz7bVtCCOylaAuh6zsxGcW9raY81O1ni11qaUPfqavvP9s3BHv/ORoeeWK36zOfC0mLgxJwjJWXoTyKp4S0cHRsS86LKWJ4W/rZ83DCvtsQTWE88+MNWT9r8GOpwBdCOSeDfutpvu0qoWqdaMtJ6jrSEwISiM8rWsRrAlGH7S9vQYZdoBWKAW/gHBWaLc0V2qlqeJBOUu20Z4/8W3Mo8PAxfjqeIp4tMMV++FY6/0IqNSeor+09Mtx/eY1VhyHazMEfvxuyyBF5P3ps5u5SSWn5By8A8E7nm8PvGmcocPT2wet4cHcYLCuKtlxbzOAZZ5J49ks/axmd48"
 }
